7f92db75baa2de1ab18050932ac770bffea76d02
LeonNguyen3 <1035777@student.sae.edu.au>
2026-04-26 20:57:26 +1000

    Fixed minor bug
    + Added colour to win box
    + Added Scene loaded to go back to main menu
    + Layers fixed

--- Assets/CreatePath/WinCondition.cs
+++ Assets/CreatePath/WinCondition.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinCondition : MonoBehaviour
 {
     public bool hasSolved = false;
 
+    private void Start()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
     public void OnTriggerEnter2D(Collider2D other)
     {
+        
         Debug.Log("Something entered the trigger: " + other.name); 
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player won");
             hasSolved = true;
+
+            SceneManager.LoadScene("MainMenu");
         }
     }
 }
